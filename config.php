4d1c9e521048d60123f3bf4e355f51ea9a444cdc
Antonio Júnior <antonio.junior.h@gmail.com>
2019-03-25 16:02:25 -0300

    deixando config do banco genérica para ambiente de produção/dev

--- config.php
+++ config.php
@@ -6,7 +6,14 @@
  * Time: 17:40
  */
 
-define('DB_USUARIO', 'root');
-define('DB_SENHA', 'nheac4257');
-define('DB_HOST', '127.0.0.1');
-define('DB_NOME', 'temigrei');
+if(gethostname() == 'servicos-internos-01.kinghost.net'){
+    define('DB_USUARIO', 'temigrei');
+    define('DB_SENHA', 'SqHZbKGZVv98v7F');
+    define('DB_HOST', 'mysql.temigrei.kinghost.com.br');
+    define('DB_NOME', 'temigrei');
+}else{
+    define('DB_USUARIO', 'root');
+    define('DB_SENHA', 'nheac4257');
+    define('DB_HOST', '127.0.0.1');
+    define('DB_NOME', 'temigrei');
+}
